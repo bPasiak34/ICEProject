f3ec9b45ff8925c2690e71601334bf05d3208934
PawelOchal <opawel262@gmail.com>
2024-03-17 14:42:08 +0100

    configure docker to set up django with postgres and configure django project

---
+++ backend/pain_information/pain_information/urls.py
@@ -0,0 +1,12 @@
+"""
+MAIN RENTRESTFUL URL Configuration
+"""
+from django.contrib import admin
+from django.urls import path
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/schema/', SpectacularAPIView.as_view(), name='api-schema'),
+    path('api/swagger/docs/', SpectacularSwaggerView.as_view(url_name='api-schema'), name='api-docs'),
+]
