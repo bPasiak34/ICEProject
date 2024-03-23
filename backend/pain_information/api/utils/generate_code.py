8a818ea65d8d91263c49c477e0d107b0b3fc92f4
PawelOchal <opawel262@gmail.com>
2024-03-23 08:45:21 +0100

    create test for django model and add func generate code as default for model pain_infromation code

--- backend/pain_information/api/utils/generate_code.py
+++ backend/pain_information/api/utils/generate_code.py
@@ -1,28 +1,19 @@
 import random
 import string
-from api.models import PainInformation
 
-def generate_code(length: int) -> str:
-    """Function used to generate a random code for the PainInformation model."""
+def generate_code(length: int = 6) -> str:
+    """
+    Function used to generate a random code for the PainInformation model.
+
+    Args:
+        length (int): The length of the generated code. Default is 6.
+
+    Returns:
+        str: A randomly generated code.
+    """
+    from api.models import PainInformation  
+
     code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
     while PainInformation.objects.filter(code=code).exists():
         code = ''.join(random.choices(string.ascii_uppercase + string.ascii_lowercase + string.digits, k=length))
     return code
-
-
-class GenerateCode:
-    def __init__(self, length: int):
-        self.length = length
-        
-    @property
-    def length(self):
-        return self._length
-
-    @length.setter
-    def length(self, value):
-        if value < 1:
-            raise ValueError("Length must be greater than or equal to 1.")
-        self._length = value
-            
-    def __call__(self):
-        return generate_code(self.length)
